40d19c440b9f0a197d09ccef0e108af8f732d62d
abir-ht00 <abirhasan.aht@gmail.com>
2022-09-08 09:50:15 +0600

    second push demo

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,15 @@ int main()
 {
     cout << "Hello World!" << endl;
 
+    string name, food;
+
+    cout << "Enter your name and food: ";
+    cin >> name >> food;
+
+    cout << "Name: " << name << endl;
+    cout << "Ordered Food: " << food << endl;
+
+    cout << "Thank you for your order!!" << endl;
+
     return 0;
 }
